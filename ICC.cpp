924e0271889ed60e5fa2cae536ec4e04c5f56e8e
Bryan McClain <bamcclain10@gmail.com>
2018-12-02 20:20:30 -0500

    Better command line return values

--- ICC.cpp
+++ ICC.cpp
@@ -8,26 +8,36 @@ extern FILE *yyin;
 int main(int argc, char** argv) {
 
 	if (argc < 2) {
-		std::cout << "Usage: " << argv[0] << " <FileName>" << std::endl;
-		return 0;
+		fprintf(stderr,"icc: Error! No input file given!\n");
+		fprintf(stderr,"Usage: %s <FileName>\n",argv[0]);
+		return 3;
 	}
 
 
 	//Open the file for reading
 	FILE* file = fopen(argv[1],"r");
 	if (file == NULL) {
-		std::cout << "Error Opening File!" << std::endl;
+		fprintf(stderr,"Error opening file '%s'!\n",argv[1]);
 		return 1;
 	}
 
 	//Get everything ready to parse
 	InsanityProgram* program = new InsanityProgram;
 	yyin = file;
-	
-	yyparse(program);
 
-	program->toProgram(stdout);
+	int result = yyparse(program);
+	fclose(file);
+
+	if (result != 0) {
+		return delete(program), 1;
+	}
+
+	//Output the program to the file
+	if (!program->toProgram(stdout)) {
+		return delete(program),2;
+	}
 	delete program;
 
+	
 	return 0;
 }
